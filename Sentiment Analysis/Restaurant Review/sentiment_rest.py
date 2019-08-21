c1a1ef258fbac587306b11bcb8972d0a9b7acbae
anshbordia <45942801+anshbordia@users.noreply.github.com>
2019-08-21 10:59:32 +1000

    Update sentiment_rest.py

--- Sentiment Analysis/Restaurant Review/sentiment_rest.py
+++ Sentiment Analysis/Restaurant Review/sentiment_rest.py
@@ -1,20 +1,22 @@
-# Natural Language Processing
+# Done as part of course Machine Learning A-Z.
 
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import confusion_matrix
 
 # Importing the dataset
 dataset = pd.read_csv('reviews.tsv', delimiter = '\t', quoting = 3)
 
 # Cleaning the texts
-import re
-import nltk
-nltk.download('stopwords')
-from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
 corpus = []
 for i in range(0, 1000):
     review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
@@ -26,7 +28,6 @@ for i in range(0, 1000):
     corpus.append(review)
 
 # Creating the Bag of Words model
-from sklearn.feature_extraction.text import CountVectorizer
 cv = CountVectorizer(max_features = 1500)
 X = cv.fit_transform(corpus).toarray()
 y = dataset.iloc[:, 1].values
@@ -35,7 +36,6 @@ y = dataset.iloc[:, 1].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
 
 # Fitting Naive Bayes to the Training set
-from sklearn.naive_bayes import GaussianNB
 classifier = GaussianNB()
 classifier.fit(X_train, y_train)
 
@@ -43,7 +43,6 @@ classifier.fit(X_train, y_train)
 y_pred = classifier.predict(X_test)
 
 # Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 print("Accuracy:", ((cm[0][0] + cm[1][1]) / (cm[0][0] + cm[0][1] + cm[1][0] + cm[1][1])))  
 
